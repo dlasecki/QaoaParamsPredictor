580f8bbd52b874ac70e188403364cb90002d8ca7
dlasecki <dario.lasecki@gmail.com>
2020-11-11 19:52:30 +0100

    Code refactoring.

--- test/vqe_solver/test_vqe_maxcut.py
+++ test/vqe_solver/test_vqe_maxcut.py
@@ -5,7 +5,7 @@ import vqe_solver.vqe_maxcut as qm
 def test_run_vqe():
     ladder_graph = gg.generate_ladder_graph(2)
     ladder_graph_weight_matrix = gg.get_weight_matrix(ladder_graph)
-    repetitions = 1
+    repetitions = 5
     max_iterations = 30
     result = qm.run_vqe(ladder_graph_weight_matrix, repetitions, max_iterations)
     assert isinstance(result.get("optimal_value"), float), "Optimal value not a float."
