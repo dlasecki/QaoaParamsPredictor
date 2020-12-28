642a27ac8ed60e3bda212f05c67b29018d9abafa
dlasecki <dario.lasecki@gmail.com>
2020-12-28 21:11:07 +0100

    Problem instance classes extended.

--- predictor/problem_instances/MaxCutProblemInstance.py
+++ predictor/problem_instances/MaxCutProblemInstance.py
@@ -6,8 +6,9 @@ from problem_instances.ProblemInstance import ProblemInstance
 
 class MaxCutProblemInstance(ProblemInstance):
 
-    def __init__(self, problem_id, p, input_graph, optimizer, num_starting_points):
-        super().__init__(problem_id, "MaxCut", p, input_graph, optimizer, num_starting_points)
+    def __init__(self, problem_id, p, input_graph, optimizer, num_starting_points, optimal_params=None, min_value=None):
+        super().__init__(problem_id, "MaxCut", p, input_graph, optimizer, num_starting_points, optimal_params,
+                         min_value)
         self.weight_operator = graphs_builder.get_weight_matrix(input_graph)
 
     def get_qaoa_operator(self):
--- predictor/problem_instances/ProblemInstance.py
+++ predictor/problem_instances/ProblemInstance.py
@@ -1,12 +1,14 @@
 class ProblemInstance:
 
-    def __init__(self, problem_id, problem_name, p, input_graph, optimizer, num_starting_points):
+    def __init__(self, problem_id, problem_name, p, input_graph, optimizer, num_starting_points, optimal_params, min_value):
         self.problem_id = problem_id
         self.problem_name = problem_name
         self.p = p
         self.input_graph = input_graph
         self.optimizer = optimizer
         self.num_starting_points = num_starting_points
+        self.optimal_params = optimal_params
+        self.min_value = min_value
 
     def get_qaoa_operator(self):
         pass
