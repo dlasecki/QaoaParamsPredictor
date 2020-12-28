77068136450b20fdc96ab011cb89c3cd2b4a0799
dlasecki <dario.lasecki@gmail.com>
2020-12-28 20:48:27 +0100

    Classes for problem instances and MaxCut class added.

---
+++ predictor/problem_instances/MaxCutProblemInstance.py
@@ -0,0 +1,14 @@
+from qiskit.optimization.applications.ising import max_cut
+
+from instances_generator import graphs_builder
+from problem_instances.ProblemInstance import ProblemInstance
+
+
+class MaxCutProblemInstance(ProblemInstance):
+
+    def __init__(self, problem_id, problem_name, p, input_graph, optimizer, num_starting_points):
+        super().__init__(problem_id, problem_name, p, input_graph, optimizer, num_starting_points)
+        self.weight_operator = graphs_builder.get_weight_matrix(input_graph)
+
+    def get_qaoa_operator(self):
+        max_cut.get_operator(self.weight_operator)
