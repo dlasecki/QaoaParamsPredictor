97a8c2839abed19ea05c91caa8d584ec33fcb10f
dlasecki <dario.lasecki@gmail.com>
2021-03-01 13:53:03 +0100

    Test updated.

--- test/qaoa/test_qaoa.py
+++ test/qaoa/test_qaoa.py
@@ -1,15 +1,15 @@
-import problem_instances.instances_generator.graphs_builder as gg
-
-from experiments.optimizers import optimizers_factory
-from experiments.problem_instances.graph_problems.specializations.MaxCutProblemInstance import MaxCutProblemInstance
+from experiments.optimizers.optimizers_factory import create_optimizer
+from experiments.problem_instances.graph_problems.specializations.maxcut_problem_instance import MaxCutProblemInstance
+from experiments.problem_instances.instances_generators.graph_problems.graphs_builder import generate_ladder_graph
+from helpers.enums import optimizer_name
 from qaoa_solver.qaoa import qaoa_with_optimizer
 
 
 def test_run_qaoa():
     problem_id = "1"
     p = 1
-    input_graph = gg.generate_ladder_graph(3)
-    optimizer = optimizers_factory.get_cobyla_optimizer()
+    input_graph = generate_ladder_graph(3, 1)
+    optimizer = create_optimizer(optimizer_name.OptimizerName.COBYLA)
     num_starting_points = 1
 
     problem_instance = MaxCutProblemInstance(problem_id, p, input_graph, optimizer, num_starting_points)
